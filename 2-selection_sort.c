ab318a376d497b13aadc5e37e2a5dbf0a14c39e0
Senyo197 <Senyo197@gmail.com>
2023-10-22 18:22:40 +0000

    selection sort algorythm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,12 +9,13 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_index;
+	int temp;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		min_index = 1;
+		min_index = i;
 
-		for (j = 1 + i; j < size; j++)
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_index])
 				min_index = j;
@@ -25,7 +26,7 @@ void selection_sort(int *array, size_t size)
 			temp = array[i];
 			array[i] = array[min_index];
 			array[min_index] = temp;
-			print_array(array, size)
+			print_array(array, size);
 		}
 	}
 }
